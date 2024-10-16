fe80bfc95ea3f5f034718b5bdffbfe7b1d5cf329
ross-lh <lhbenross@gmail.com>
2024-10-16 01:46:59 -0300

    updating codes folder - exercise files inside

---
+++ codes/src/pokemini.js
@@ -0,0 +1,17 @@
+// variáveis de stats pokémon
+let id = 'Pikachu'
+let level = 20 
+let hp = 40
+let hpmax = 45 
+let gender = 'male' 
+let availability = true
+
+function(string) {
+    if (availability = true): return string='disponível' 
+}
+
+console.log('tu tem um(a):' + id
+    + " level: " + level + " com "
+    + hp + "/" + hpmax + " de HP "    
+    + gender + "que está "
+)
---
+++ codes/src/vovo.js
@@ -0,0 +1,19 @@
+//declaração de variáveis p dados da vovó
+let nomeVovo = 'Joseva'
+let idadeVovo = 115
+let telefoneVovo = '(11)95833-4940'
+let cpfVovo = '123.456.789-00'
+let enderecoVovo = 'Rua dos Tralha, 40, bairro DaFé'
+let ehAposentada = true
+
+//testando declaração de vetores e matrizes
+let pokemon = [
+    ["charmander",10,"male"],
+    ["gyarados",25,"male"],
+    ["blastoise",40,"female"],
+]
+
+//testando call de valores em matrizes
+console.log(pokemon[0][0])
+console.log(pokemon[1][0])
+console.log(pokemon[2][0])
