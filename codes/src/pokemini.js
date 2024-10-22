f002b2ac92720db9d22ef9d7d2589483d2fad41c
ross-lh <lhbenross@gmail.com>
2024-10-22 00:05:21 -0300

    new features added to pokemini.js

--- codes/src/pokemini.js
+++ codes/src/pokemini.js
@@ -1,11 +1,18 @@
 // variáveis de stats pokémon
-let id = 'Pikachu'
+let id = 'Sparrow'
 let level = 20 
 let hp = 40
 let hpmax = 45 
-let gender = 'macho' 
+let gender = 'fêmea' 
 let availability = true
 let requiredPokemon = "Charmander"
+
+// verificando o artigo a ser usado para o Pokémon cadastrado
+if(gender == "male"){
+    let proNoun = "ele"
+}else{
+    let proNoun = "ela"
+}
 
 // verificando disponibilidade do Pokémon
 let statusA = ""
@@ -13,19 +20,28 @@ let statusA = ""
 if(availability === true){
 statusA = "disponível"
 }else{
-statusA = "fainted"
+statusA = "derrotado :("
 }
 
-console.log('tu tem um(a): ' + id
-    + " level: " + level + " com "
-    + hp + "/" + hpmax + " de HP, "    
-    + gender + ", que está " + statusA + ".")
+console.log('Você tem um ' + id +" "+ gender
+    + ", level " + level + " com "
+    + hp + "/" + hpmax + " de HP, que está " + statusA + ".")
 console.log()
 
 // testando operadores lógicos
 let verifyPokemon = (id == requiredPokemon)
 
-console.log(verifyPokemon)
-console.log()
-console.log(statusA)
-console.log(!statusA)
+//testando sintaxe e funcionalidades switch-case
+switch (id){
+    case "Charmander":
+        console.log("Que belo "+id+", hein!")
+        break
+
+    case "Pidgey":
+    case "Sparrow":
+        console.log("Que bela "+"galinha"+", hein!")
+        break
+
+    default:
+        console.log("Que belo Pokébicho, hein!")
+}
